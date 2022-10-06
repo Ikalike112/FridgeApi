8f9955812a49db6a30da82b56bff47bbd69b548d
Дмитрий Корец <dimon.korecz@gmail.com>
2022-10-06 10:47:38 +0300

    Add models

---
+++ Domain/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Products
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int DefaultQuantity { get; set; }
+        public ICollection<FridgeProducts> FridgeProducts { get; set; }
+    }
+}
